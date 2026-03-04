f84d873d6d18cec2cb56e9b0bec0d83e7c4a6820
Idris Raja <idris.raja@gmail.com>
2026-03-04 14:44:38 -0600

    simplify, bump

--- flake.nix
+++ flake.nix
@@ -1,33 +1,16 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" "aarch64-darwin" ]
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           pdftc = pkgs.callPackage ./default.nix { };
-          benchApp = pkgs.writeShellApplication {
-            name = "pdftc-bench";
-            runtimeInputs = [
-              pkgs.bash
-              pkgs.coreutils
-              pkgs.hyperfine
-              pdftc
-            ];
-            text = ''
-              if [ ! -f "$PWD/bench/bench.sh" ]; then
-                echo "bench/bench.sh not found; run from repo root" >&2
-                exit 1
-              fi
-
-              export PDFTC_ROOT="$PWD"
-              exec "$PWD/bench/bench.sh"
-            '';
-          };
         in
         {
           packages = {
@@ -35,20 +18,11 @@
             pdftc = pdftc;
           };
           checks.default = pdftc;
-          apps = {
-            default = flake-utils.lib.mkApp { drv = pdftc; };
-            pdftc = flake-utils.lib.mkApp { drv = pdftc; };
-            bench = flake-utils.lib.mkApp { drv = benchApp; };
-          };
-          devShells.default = pkgs.mkShell {
-            packages = [
-              pkgs.coreutils
-              pkgs.gnused
-              pkgs.hyperfine
-              pkgs.pdftk
-              (pkgs.python3.withPackages (ps: [ ps.reportlab ]))
-              pkgs.ripgrep
-            ];
+          apps = rec {
+            default = pdftc;
+            pdftc = flake-utils.lib.mkApp {
+              drv = pdftc;
+            };
           };
         }) //
     {
